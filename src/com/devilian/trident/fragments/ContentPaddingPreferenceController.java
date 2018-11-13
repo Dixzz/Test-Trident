ff7a2265dee5fc3f00032e89aa0c0f8d4090ad1b
Satyam Sharma <33489151+Dixzz@users.noreply.github.com>
2018-11-13 13:26:21 +0530

    Update ContentPaddingPreferenceController.java

--- src/com/devilian/trident/fragments/ContentPaddingPreferenceController.java
+++ src/com/devilian/trident/fragments/ContentPaddingPreferenceController.java
@@ -11,7 +11,7 @@
  * KIND, either express or implied. See the License for the specific language governing
  * permissions and limitations under the License.
  */
-package com.devilian.settings.fragments;
+package com.devilian.trident.fragments;
 
 import android.content.Context;
 import android.content.ContentResolver;
